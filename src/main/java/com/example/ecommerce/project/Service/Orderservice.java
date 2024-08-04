a961243ac7317677474e295ec3decce0ee863e9d
mahdad1372 <mnaderiniya@gmail.com>
2024-08-04 16:44:37 +0200

    adding the order service

---
+++ src/main/java/com/example/ecommerce/project/Service/Orderservice.java
@@ -0,0 +1,34 @@
+package com.example.ecommerce.project.Service;
+
+import com.example.ecommerce.project.Entity.Categories;
+import com.example.ecommerce.project.Entity.Orders;
+import com.example.ecommerce.project.Entity.Products;
+import com.example.ecommerce.project.Repository.CategoryRepository;
+import com.example.ecommerce.project.Repository.OrderRepository;
+import com.example.ecommerce.project.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class Orderservice {
+    @Autowired
+    private OrderRepository orderRepository;
+    public List<Orders> getAllOrders(){
+        return orderRepository.getAllOrders();
+    }
+    public List<Orders> getOrdersById(Integer id){
+        return orderRepository.getOrdersById(id);
+    }
+    public void deleteOrderByOrderId(Integer id){
+        orderRepository.deleteOrderByOrderId(id);
+    }
+    public void updateStatusById(Integer id,String status){
+        orderRepository.updateStatusById(id,status);
+    }
+
+    public void addOrder(Integer user_id,Integer total_amount,String status){
+        orderRepository.addOrder(user_id,total_amount,status);
+    }
+}
+
